41ed84994abe5573918f2f173887f6a923fe2418
iHakim <ettakhi@gmail.com>
2019-05-12 19:10:12 +0200

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = str => {
+  const list = {};
+  let [maxL, maxC] = [0, str[0]];
+  for (const c of str) {
+    list[c] = (list[c] + 1) | 1;
+    if (list[c] > maxL) [maxL, maxC] = [list[c], c];
+  }
+  return maxC;
+};
 
 module.exports = maxChar;
