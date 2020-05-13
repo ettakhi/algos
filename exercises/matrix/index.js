98ec4f9aed4a6f63e1f0570fa8c53918bfd8c339
iHakim <ettakhi@gmail.com>
2020-05-13 04:53:14 +0200

    resolve matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+const matrix = (n) => {
+  const mtrx = Array(n);
+  for (let i = 0; i < n; i++) {
+    mtrx[i] = Array(n).fill(null);
+  }
+
+  let step = 1;
+  let direction = "right";
+  let [i, j] = [0, 0];
+  while (true) {
+    mtrx[i][j] = step;
+    step++;
+    if (direction === "right") {
+      if (j === n - 1 || mtrx[i][j + 1] !== null) {
+        i++;
+        direction = "down";
+        if (mtrx[i][j]) break;
+      } else {
+        j++;
+      }
+    } else if (direction === "down") {
+      if (i === n - 1 || mtrx[i + 1][j] !== null) {
+        j--;
+        direction = "left";
+        if (mtrx[i][j]) break;
+      } else {
+        i++;
+      }
+    } else if (direction === "left") {
+      if (j === 0 || mtrx[i][j - 1] !== null) {
+        i--;
+        direction = "up";
+        if (mtrx[i][j]) break;
+      } else {
+        j--;
+      }
+    } else if (direction === "up") {
+      if (i === 0 || mtrx[i - 1][j] !== null) {
+        j++;
+        direction = "right";
+        if (mtrx[i][j]) break;
+      } else {
+        i--;
+      }
+    }
+  }
+  return mtrx;
+};
+matrix(4);
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,16 +6,21 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
-const vows = "aeiou";
-const vowels = (str) => {
-  let count = 0;
-  for (const c of str.toLowerCase()) {
-    if (vows.includes(c)) count++;
-  }
-  return count;
-};
+
+// const vowels = (str) => {
+//   const vows = "aeiou";
+//   let count = 0;
+//   for (const c of str.toLowerCase()) {
+//     if (vows.includes(c)) count++;
+//   }
+//   return count;
+// };
+
 // const vowels = (str) => {
 //   return [...str.toLowerCase()].reduce((count, c) => (vows.includes(c) ? count + 1 : count), 0);
 // };
-
+const vowels = (str) => {
+  const matchs = str.match(/[aeiou]/gi);
+  return matchs ? matchs.length : 0;
+};
 module.exports = vowels;
